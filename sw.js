9b175bf61c83cfa810b5617aeff785420b31dfdd
MM050692 <amonlinefru247@gmail.com>
2026-08-28 00:13:03 +0500

    Refresh Marathi help cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'our-dhan-v9-5';
+const CACHE = 'our-dhan-v9-6';
 const CORE = [
   './', './index.html', './style.css', './app.js', './config.js', './manifest.json',
   './icon.svg', './icon-192.png', './icon-512.png', './apple-touch-icon.png',
