0516e6172ce0559c2e8e73b58b234a28457475de
MM050692 <amonlinefru247@gmail.com>
2026-08-27 18:02:17 +0500

    Refresh Our DHAN app cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'our-dhan-v9-2';
+const CACHE = 'our-dhan-v9-3';
 const CORE = [
   './', './index.html', './style.css', './app.js', './config.js', './manifest.json',
   './icon.svg', './icon-192.png', './icon-512.png', './apple-touch-icon.png',
